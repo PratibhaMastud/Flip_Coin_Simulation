04198c66e066c2855a12f595ae9b36612813b0f3
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-20 19:22:20 +0530

    Added:Flip the coin 10 times and find who is win head or tail

--- filpCoinSimulator.sh
+++ filpCoinSimulator.sh
@@ -1,11 +1,31 @@
 #! /bin/bash
 #Author:PratibhaMastud
-#1.Display Heads or Tails as winner by Flipping a Coin
+#1.Check how many times head or tails win by flipping 10 times.
 
-Flip=$((RANDOM%2))
-if [ $Flip -eq 1 ]
-then
-	echo "Head is Winner"
-else
-	echo "Tail is Winner"
+#variable initialization
+times=10
+i=0
+head=0
+tail=0
+
+while [ $i -lt $times ]
+do
+        flip=$((RANDOM%2))
+
+        if [ $flip -eq 1 ]
+        then
+                head=$(($head+1))
+        else
+                tail=$(($tail+1))
+        fi
+
+        i=$(($i+1))
+done
+# we are going to check who is win by comparing the score
+if [ $head -gt $tail ]
+        then
+                echo "Winner is Head with $head Numbers"
+        else
+                echo "Winner is Tail with $tail Numbers"
 fi
+
