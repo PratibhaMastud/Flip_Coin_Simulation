85491ee73515ad14b1454114a85166e418f6fd65
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-20 19:06:31 +0530

    Added:Flip the coin and display Head or tail as winner

--- filpCoinSimulator.sh
+++ filpCoinSimulator.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 #Author:PratibhaMastud
-#1.Display Heads or Tails as winner by Flipping a Coin
+#2.Display Heads or Tails as winner by Flipping a Coin
 
-Flip=$((RANDOM%2))
-if [ $Flip -eq 1 ]
+head=1
+# We use RANDOM number for flipping coin
+flip=$((RANDOM%2))
+
+#Now we check the winner head or tail
+if [ $head -eq 1 ]
 then
-	echo "Head is Winner"
+	echo "The Winner is Head "
 else
-	echo "Tail is Winner"
+                echo "The Winner is Tail "
 fi
